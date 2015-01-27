91cd9f95b97bc508ad5d8fcb1995e24b29b1410f
alltonp <paulallton@gmail.com>
2015-01-27 13:55:54 +0000

    migrate select specs from specs2 to scalatest

--- src/test/scala/im/yagni/acceptance/driveby/scalatest/BrowserSpec.scala
+++ src/test/scala/im/yagni/acceptance/driveby/scalatest/BrowserSpec.scala
@@ -1,7 +1,5 @@
 package im.yagni.acceptance.driveby.scalatest
 
-import im.yagni.driveby.Id
-import im.yagni.driveby.conditions._
 import org.scalatest.Matchers
 
 class BrowserSpec extends WebSpecification with Matchers {
@@ -25,38 +23,11 @@ class BrowserSpec extends WebSpecification with Matchers {
 //  }
   //template-ends
 
-    def `select must select an option` {
-      val id = Id("select")
-      given.page(<body><form><select id={id.id}><option>1</option><option>2</option></select></form></body>)
-        .select(id, "2")
-        .assert(ValueEquals(id, "2"))
-    }
 
 //   "" should {
 //
-//     "only select if element can be uniquely identified" in rawBrowser {
-//       (given: BrowserDriver) => {
-//         val id = Id("select")
-//         given.page(<body><form><select id={id.id}/><br/><select id={id.id}/></form></body>)
-//           .select(id, "blah") must throwA(new ConditionNotMetException("""> FAILED: select 'Id(select)' option 'blah' (not met within 2000 millis)"""))
-//       }
-//     }
 //
-//     "only select if element is interactable" in rawBrowser {
-//       (given: BrowserDriver) => {
-//         val id = Id("select")
-//         given.page(<body><form><select id={id.id} style="display: none"/></form></body>)
-//           .select(id, "blah") must throwA(new ConditionNotMetException("""> FAILED: select 'Id(select)' option 'blah' (not met within 2000 millis)"""))
-//       }
-//     }
 //
-//     "only select if option is present" in rawBrowser {
-//       (given: BrowserDriver) => {
-//         val id = Id("select")
-//         given.page(<body><form><select id={id.id}><option>1</option></select></form></body>)
-//           .select(id, "2") must throwA(new ConditionNotMetException("""> FAILED: select 'Id(select)' option '2' (not met within 2000 millis)"""))
-//       }
-//     }
 //   }
 //
 //   "html" should {
