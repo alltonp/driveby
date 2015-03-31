d2d658fa42f8807a173b7b0648b671e074e7f37a
alltonp <paulallton@gmail.com>
2015-03-31 12:42:24 +0100

    make root of Spec be Suite - just for Franck

--- src/main/scala/im/mange/driveby/scalatest/DriveBySpecification.scala
+++ src/main/scala/im/mange/driveby/scalatest/DriveBySpecification.scala
@@ -1,10 +1,9 @@
 package im.mange.driveby.scalatest
 
-import org.scalatest.Spec
 import im.mange.driveby.tracking.{ExampleTracking, SpecificationTracking}
-import im.mange.driveby.BrowserAware
+import org.scalatest.Suite
 
 //TODO: with ErrorReporting
 trait DriveBySpecification extends NakedSpecification with NakedExample with SpecificationTracking with ExampleTracking with DriveByReporting {
-  this: Spec =>
+  this: Suite =>
 }
