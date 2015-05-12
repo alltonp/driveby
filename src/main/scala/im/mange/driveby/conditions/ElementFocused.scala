10651f9bd701caba7788a0d9e3cc9bc27c92ff05
alltonp <paul.allton@theorem.technology>
2015-05-12 17:01:06 +0100

    thanks intellij

--- src/main/scala/im/mange/driveby/conditions/ElementFocused.scala
+++ src/main/scala/im/mange/driveby/conditions/ElementFocused.scala
@@ -1,6 +1,10 @@
 package im.mange.driveby.conditions
 
+import im.mange.driveby.browser.UnSafeBrowser
+import im.mange.driveby.{By, Condition, Describer}
+
 case class ElementFocused(by: By) extends Condition {
+  import Describer._
 
   def expectation = expect("ElementFocused", List(by.toString))
   def isSatisfied(browser: UnSafeBrowser) = { browser.hasFocus(by) == true }
