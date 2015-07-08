55191cb9c1a527fd62d400315dd369ded1c77e82
alltonp <paulallton@gmail.com>
2015-07-08 09:06:02 +0100

    rename again

--- src/test/scala/im/mange/acceptance/driveby/scalatest/condition/ElementCheckedSpec.scala
+++ src/test/scala/im/mange/acceptance/driveby/scalatest/condition/ElementCheckedSpec.scala
@@ -8,16 +8,16 @@ import org.scalatest.Matchers
 
 class ElementCheckedSpec extends WebSpecification with Matchers {
   def `pass for id when checked - checkbox` {
-    val id = Id("ElementChecked-Checkbox")
+    val id = Id("ElementCheckedCheckbox")
     given.page(<body><form><input type="checkbox" checked="checked" id={id.id}/></form></body>)
       .assert(ElementChecked(id))
   }
 
   def `fail for id not checked - checkbox` {
-    val id = Id("ElementCheckedWhenNotChecked-Checkbox")
+    val id = Id("ElementCheckedWhenNotCheckedCheckbox")
     val b = given.page(<body><form><input type="checkbox" id={id.id}/></form></body>)
 
     val thrown = the [ConditionNotMetException] thrownBy { b.assert(ElementChecked(id)) }
-    thrown.getMessage should equal("""> FAILED: Assert ElementChecked("Id(ElementCheckedCheckboxWhenNotChecked)") but was "null"  (not met within 2000 millis)""")
+    thrown.getMessage should equal("""> FAILED: Assert ElementChecked("Id(ElementCheckedWhenNotCheckedCheckbox)") but was "null"  (not met within 2000 millis)""")
   }
 }
