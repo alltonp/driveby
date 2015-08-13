a55f66af97cccc0d4e45a32a972fd334f177ebce
alltonp <paulallton@gmail.com>
2015-08-13 10:16:32 +0100

    add missing example

--- src/test/scala/im/mange/acceptance/driveby/scalatest/condition/ElementHiddenSpec.scala
+++ src/test/scala/im/mange/acceptance/driveby/scalatest/condition/ElementHiddenSpec.scala
@@ -1,14 +1,24 @@
 package im.mange.acceptance.driveby.scalatest.condition
 
 import im.mange.acceptance.driveby.scalatest.WebSpecification
+import im.mange.common.ConditionNotMetException
 import im.mange.driveby.Id
 import im.mange.driveby.conditions._
 import org.scalatest.Matchers
 
 class ElementHiddenSpec extends WebSpecification with Matchers {
   def `work for id` {
-    val id = Id("hidden")
-    given.page(<b id={id.id} style="display:none">hidden</b>)
-      .assert(ElementHidden(id))
+    val id = "ElementHidden"
+    given.page(<b id={id} style="display:none">{id}</b>)
+      .assert(ElementHidden(Id(id)))
   }
+
+  def `fail for id not hidden` {
+    val id = "ElementHiddenWhenNotHidden"
+    val b = given.page(<b id={id} style="">{id}</b>)
+
+    val thrown = the [ConditionNotMetException] thrownBy { b.assert(ElementHidden(Id(id))) }
+    thrown.getMessage should equal("""> FAILED: Assert ElementHidden("Id(ElementHiddenWhenNotHidden)", "true") but was "true"  (not met within 2000 millis)""")
+  }
+
 }
